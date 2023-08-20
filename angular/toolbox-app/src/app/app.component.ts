97c37c6213bf6fa9f572862efcdaa14793f64ab4
Vlad Berezan <vlad.berezan@externals.adidas.com>
2023-08-20 12:31:32 +0300

    init

--- angular/toolbox-app/src/app/app.component.ts
+++ angular/toolbox-app/src/app/app.component.ts
@@ -1,20 +1,10 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  ngOnInit(): void {
-    console.log('----------------------------------------------------------------');
-    console.log('---------------------- DarkGalaxy Toolbox ----------------------');
-    console.log('----------------------------------------------------------------');
-    console.log('------------------------ Main Features: ------------------------');
-    console.log('----- 1. Fix styling glitches ----------------------------------');
-    console.log('----- 2. Provide a modern alternative to the game header  ------');
-    console.log('----- 2. Provide a modern alternative to the game header  ------');
-    console.log('----------------------------------------------------------------');
-  }
+export class AppComponent {
 
 }
--- angular/toolbox-app/src/app/app.module.ts
+++ angular/toolbox-app/src/app/app.module.ts
@@ -1,7 +1,7 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 
 @NgModule({
   declarations: [
@@ -13,4 +13,15 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor() {
+    console.log('----------------------------------------------------------------');
+    console.log('---------------------- DarkGalaxy Toolbox ----------------------');
+    console.log('----------------------------------------------------------------');
+    console.log('------------------------ Main Features: ------------------------');
+    console.log('----- 1. Fix styling glitches ----------------------------------');
+    console.log('----- 2. Provide a modern alternative to the game header  ------');
+    console.log('----- 2. Provide a modern alternative to the game header  ------');
+    console.log('----------------------------------------------------------------');
+  }
+}
