3edd3a2f05ab4e7127d40071daa83014a61dbf93
Vlad Berezan <vlad.berezan@externals.adidas.com>
2023-08-20 09:39:47 +0300

    init

--- angular/toolbox-app/src/app/app.component.ts
+++ angular/toolbox-app/src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'toolbox-app';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    console.log('------------------------------------------------')
+    console.log('----- DarkGalaxy Toolbox v0.1 Beta enabled -----');
+    console.log('------------------------------------------------')
+  }
+
 }
