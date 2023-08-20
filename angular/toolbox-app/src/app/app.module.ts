f19f10ae74788ee47d1476a1ea08e6a270d76914
Vlad Berezan <vlad.berezan@externals.adidas.com>
2023-08-20 13:13:40 +0300

    init

--- angular/toolbox-app/src/app/app.module.ts
+++ angular/toolbox-app/src/app/app.module.ts
@@ -14,14 +14,14 @@ import {AppComponent} from './app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule {
-  // constructor() {
-  //   console.log('----------------------------------------------------------------');
-  //   console.log('---------------------- DarkGalaxy Toolbox ----------------------');
-  //   console.log('----------------------------------------------------------------');
-  //   console.log('------------------------ Main Features: ------------------------');
-  //   console.log('----- 1. Fix styling glitches ----------------------------------');
-  //   console.log('----- 2. Provide a modern alternative to the game header  ------');
-  //   console.log('----- 2. Provide a modern alternative to the game header  ------');
-  //   console.log('----------------------------------------------------------------');
-  // }
+  constructor() {
+    console.log('----------------------------------------------------------------');
+    console.log('---------------------- DarkGalaxy Toolbox ----------------------');
+    console.log('----------------------------------------------------------------');
+    console.log('------------------------ Main Features: ------------------------');
+    console.log('----- 1. Fix styling glitches ----------------------------------');
+    console.log('----- 2. Provide a modern alternative to the game header  ------');
+    console.log('----- 2. Provide a modern alternative to the game header  ------');
+    console.log('----------------------------------------------------------------');
+  }
 }
