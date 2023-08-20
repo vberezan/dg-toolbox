7cf6f2ee7a225e07257c967fa8fb4783898ee2fb
Vlad Berezan <vlad.berezan@externals.adidas.com>
2023-08-20 12:59:56 +0300

    init

--- angular/toolbox-app/src/main.ts
+++ angular/toolbox-app/src/main.ts
@@ -1,8 +1,8 @@
-import 'zone.js';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
 import {AppModule} from './app/app.module';
 
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
   .catch(err => console.error(err));
