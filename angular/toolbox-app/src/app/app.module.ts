a54fa0652497138bd6fde7168a3dfb8ebc78b323
Vlad Berezan <vlad.berezan@externals.adidas.com>
2023-08-20 22:07:26 +0300

    remove metronic angular support

--- angular/toolbox-app/src/app/app.module.ts
+++ angular/toolbox-app/src/app/app.module.ts
@@ -2,13 +2,20 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
+import {NavbarModule} from "./modules/navbar/navbar.module";
+import {RouterLink, RouterLinkActive} from "@angular/router";
+import {TranslateModule} from "@ngx-translate/core";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NavbarModule,
+    RouterLink,
+    RouterLinkActive,
+    TranslateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
@@ -20,8 +27,7 @@ export class AppModule {
     console.log('----------------------------------------------------------------');
     console.log('------------------------ Main Features: ------------------------');
     console.log('----- 1. Fix styling glitches ----------------------------------');
-    console.log('----- 2. Provide a modern alternative to the game header  ------');
-    console.log('----- 2. Provide a modern alternative to the game header  ------');
+    console.log('----- 2. Provide a modern alternative to the game navbar  ------');
     console.log('----------------------------------------------------------------');
   }
 }
