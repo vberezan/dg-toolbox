8b88cee5d1094baef9bcf5c46bf37ac1b9753899
Vlad Berezan <vlad.berezan@externals.adidas.com>
2023-08-22 11:46:10 +0300

    work at navbar

--- angular/toolbox-app/src/app/modules/application-routing/application-routing.module.ts
+++ angular/toolbox-app/src/app/modules/application-routing/application-routing.module.ts
@@ -1,21 +1,71 @@
 import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from "@angular/router";
+import {ActivatedRouteSnapshot, RouterModule, RouterStateSnapshot, Routes} from "@angular/router";
 import {MenuComponent} from "../navbar/component/menu-replacement/menu.component";
 
 
 const routes: Routes = [{
     path: '', children: [
-        {path: 'planets', component: MenuComponent},
-        {path: 'radar', component: MenuComponent},
-        {path: 'fleets', component: MenuComponent},
-        {path: 'navigation', component: MenuComponent},
-        {path: 'research', component: MenuComponent},
-        {path: 'alliances', component: MenuComponent}
+        {
+            path: 'planets', component: MenuComponent, resolve: {
+                url: 'externalUrlRedirectResolver'
+            },
+            data: {
+                externalUrl: '/planets'
+            }
+        },
+        {
+            path: 'radar', component: MenuComponent, resolve: {
+                url: 'externalUrlRedirectResolver'
+            },
+            data: {
+                externalUrl: '/radar'
+            }
+        },
+        {
+            path: 'fleets', component: MenuComponent, resolve: {
+                url: 'externalUrlRedirectResolver'
+            },
+            data: {
+                externalUrl: '/fleets'
+            }
+        },
+        {
+            path: 'navigation', component: MenuComponent, resolve: {
+                url: 'externalUrlRedirectResolver'
+            },
+            data: {
+                externalUrl: '/navigation'
+            }
+        },
+        {
+            path: 'research', component: MenuComponent, resolve: {
+                url: 'externalUrlRedirectResolver'
+            },
+            data: {
+                externalUrl: '/research'
+            }
+        },
+        {
+            path: 'alliances', component: MenuComponent, resolve: {
+                url: 'externalUrlRedirectResolver'
+            },
+            data: {
+                externalUrl: '/alliances'
+            }
+        }
     ]
 }];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
+    providers: [{
+        provide: 'externalUrlRedirectResolver',
+        useValue: (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
+            if (window.location.pathname != (route.data as any).externalUrl) {
+                window.location.href = (route.data as any).externalUrl;
+            }
+        }
+    }],
     exports: [RouterModule]
 })
 export class ApplicationRoutingModule {
