35982acfd8f63cdd6d128270316b14f1be102c82
Vlad Berezan <vlad.berezan@externals.adidas.com>
2023-08-24 23:31:18 +0300

    finish planet list

--- angular/toolbox-app/src/main.ts
+++ angular/toolbox-app/src/main.ts
@@ -7,9 +7,9 @@ const platform = platformBrowserDynamic();
 let windowURL = window.location.pathname.split(/\//g);
 platform.bootstrapModule(NavbarModule).catch(err => console.error(err));
 
-// if (windowURL[1] === 'planets') {
-//   platform.bootstrapModule(PlanetsListStatsModule).catch(err => console.error(err));
-// }
+if (windowURL[1] === 'planets') {
+  platform.bootstrapModule(PlanetsListStatsModule).catch(err => console.error(err));
+}
 
 if (windowURL[1] === 'planet' && (windowURL.length === 5 && windowURL[3]) === 'comms') {
   platform.bootstrapModule(ScansInCloudModule).catch(err => console.error(err));
