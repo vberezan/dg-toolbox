dd80d8cc09015d8a197f202fa2fa4b9679a8d865
Vlad Berezan <vlad.berezan@externals.adidas.com>
2023-09-08 18:43:13 +0300

    work at scans

--- angular/toolbox-app/src/app/modules/navigation-scans/navigation-scans.module.ts
+++ angular/toolbox-app/src/app/modules/navigation-scans/navigation-scans.module.ts
@@ -15,6 +15,8 @@ import {ScanDataPanelComponent} from './component/scan-data-panel/scan-data-pane
   ]
 })
 export class NavigationScansModule implements OnInit {
+  constructor() {
+  }
   ngOnInit(): void {
     console.log("%cDGT%c - installed navigation scans panel...", "font-size: 12px; font-weight: bold;", "font-size: 12px;");
   }
