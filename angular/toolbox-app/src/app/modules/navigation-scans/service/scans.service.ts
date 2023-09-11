74afe3e90f5b98e43ec88a9175f3af5ff1e5aff5
Vlad Berezan <vlad.berezan@externals.adidas.com>
2023-09-11 11:47:35 +0300

    work at scans

--- angular/toolbox-app/src/app/modules/navigation-scans/service/scans.service.ts
+++ angular/toolbox-app/src/app/modules/navigation-scans/service/scans.service.ts
@@ -70,8 +70,8 @@ export class ScansService {
           }
 
           if (byLocation.get(planetLocation).workers.currentNumber > 0) {
-            planet.querySelector('.dgt-navigation-scan-population').removeStyle('display');
-            planet.querySelector('.navigation .row .planets .dgt-navigation-scan-invasion-data').removeStyle('display');
+            planet.querySelector('.dgt-navigation-scan-population').style.display = '';
+            planet.querySelector('.navigation .row .planets .dgt-navigation-scan-invasion-data').style.display = '';
           }
         } else {
           planet.querySelector('.dgt-navigation-scan-turn .dgt-navigation-scan-turn-value').textContent = 'N/A';
