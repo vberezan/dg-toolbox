03cc12dfaf492e3274ea877559f5afd6b5ad1958
Vlad Berezan <vlad.berezan@externals.adidas.com>
2023-09-12 20:14:13 +0300

    new structures

--- angular/toolbox-app/src/app/modules/authentication/component/login/login.component.ts
+++ angular/toolbox-app/src/app/modules/authentication/component/login/login.component.ts
@@ -1,11 +1,17 @@
-import {Component, inject} from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {AuthService} from "../../service/auth.service";
+import {DOCUMENT} from "@angular/common";
 
 @Component({
     selector: 'dgt-authentication',
     templateUrl: './login.component.html',
     styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+    private document: any = inject(DOCUMENT);
     authService: AuthService = inject(AuthService);
+
+    ngOnInit(): void {
+        document.getElementById('dgt-login').style.display = '';
+    }
 }
