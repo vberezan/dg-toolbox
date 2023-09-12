965047017dc52aa566a2966be145cfb78da86c5a
Vlad Berezan <vlad.berezan@externals.adidas.com>
2023-09-12 20:23:50 +0300

    new structures

--- angular/toolbox-app/src/app/modules/authentication/component/login/login.component.ts
+++ angular/toolbox-app/src/app/modules/authentication/component/login/login.component.ts
@@ -1,18 +1,12 @@
-import {AfterViewInit, Component, inject, OnInit} from '@angular/core';
+import {AfterViewInit, Component, inject} from '@angular/core';
 import {AuthService} from "../../service/auth.service";
-import {DOCUMENT} from "@angular/common";
 
 @Component({
     selector: 'dgt-authentication',
     templateUrl: './login.component.html',
     styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements AfterViewInit {
-    private document: any = inject(DOCUMENT);
+export class LoginComponent {
     authService: AuthService = inject(AuthService);
-
-    ngAfterViewInit(): void {
-        document.getElementById('dgt-login').style.display = '';
-        document.getElementById('dgt-logout').style.display = '';
-    }
+    protected readonly localStorage = localStorage;
 }
