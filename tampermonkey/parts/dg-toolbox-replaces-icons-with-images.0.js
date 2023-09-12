6932a5ee197c8843276082616778bb6426845c1f
Vlad Berezan <vlad.berezan@externals.adidas.com>
2023-09-12 13:32:47 +0300

    new structures

---
+++ tampermonkey/parts/dg-toolbox-replaces-icons-with-images.0.js
@@ -0,0 +1,15 @@
+function replaceIconsWithImages() {
+    replaceImgWithImg('/images/units/small/soldier.png', 'https://i.imgur.com/FHlHgxL.png');
+    replaceImgWithImg('/images/units/small/soldier.gif', 'https://i.imgur.com/FHlHgxL.png');
+    replaceImgWithImg('/images/units/small/worker.png', 'https://i.imgur.com/7u6VnpE.png');
+    replaceImgWithImg('/images/units/small/worker.gif', 'https://i.imgur.com/7u6VnpE.png');
+    replaceImgWithImg('/images/units/small/food.gif', 'https://i.imgur.com/7gmDfoF.png');
+    replaceImgWithImg('/images/units/small/metal.gif', 'https://i.imgur.com/FPuGoOV.png');
+    replaceImgWithImg('/images/units/small/mineral.gif', 'https://i.imgur.com/OTTk4eY.png');
+    replaceImgWithImg('/images/units/small/energy.gif', 'https://i.imgur.com/DEv8NW9.png');
+    replaceImgWithImg('/images/units/small/ground.gif', 'https://i.imgur.com/HesibsH.png');
+    replaceImgWithImg('/images/units/small/orbit.gif', 'https://i.imgur.com/KKdM7BA.png');
+    replaceImgWithImg('/images/units/small/coords.gif', 'https://i.imgur.com/F0Vdyzn.png');
+
+    console.log("%cDGT%c - installed new icons...", "font-size: 12px; font-weight: bold;", "font-size: 12px;");
+}
