47702e71d2eb8ce1f565a026447562e2fc5b4b71
Vlad Berezan <vlad.berezan@externals.adidas.com>
2023-09-13 12:36:07 +0300

    new structures

--- angular/toolbox-app/src/app/modules/authentication/service/auth.service.ts
+++ angular/toolbox-app/src/app/modules/authentication/service/auth.service.ts
@@ -7,7 +7,7 @@ import {AngularFirestore} from "@angular/fire/compat/firestore";
 import GoogleAuthProvider = firebase.auth.GoogleAuthProvider;
 
 @Injectable({
-    providedIn: 'platform'
+    providedIn: 'root'
 })
 export class AuthService implements OnDestroy {
     private auth: AngularFireAuth = inject(AngularFireAuth);
