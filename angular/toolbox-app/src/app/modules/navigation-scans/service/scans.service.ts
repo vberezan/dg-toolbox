d83892440895b3ac6c3b272a14bf96479c4a1479
Vlad Berezan <vlad.berezan@externals.adidas.com>
2023-09-18 23:07:21 +0300

    Cleanup

--- angular/toolbox-app/src/app/modules/navigation-scans/service/scans.service.ts
+++ angular/toolbox-app/src/app/modules/navigation-scans/service/scans.service.ts
@@ -108,11 +108,11 @@ export class ScansService {
             planet.querySelector('.dgt-navigation-scan-structures').style.display = '';
           }
         } else {
-          planet.querySelector('.dgt-navigation-scan-turn .dgt-navigation-scan-turn-value').textContent = 'N/A';
-          planet.querySelector('.dgt-navigation-scan .dgt-navigation-scan-resource.metal .abundance').textContent = '-';
-          planet.querySelector('.dgt-navigation-scan .dgt-navigation-scan-resource.mineral .abundance').textContent = '-';
-          planet.querySelector('.dgt-navigation-scan .dgt-navigation-scan-resource.food .abundance').textContent = '-';
-          planet.querySelector('.dgt-navigation-scan .dgt-navigation-scan-resource.energy .abundance').textContent = '-';
+          planet.querySelector('.dgt-navigation-scan-turn-value').textContent = 'N/A';
+          planet.querySelector('.dgt-navigation-scan-resource.metal .abundance').textContent = '-';
+          planet.querySelector('.dgt-navigation-scan-resource.mineral .abundance').textContent = '-';
+          planet.querySelector('.dgt-navigation-scan-resource.food .abundance').textContent = '-';
+          planet.querySelector('.dgt-navigation-scan-resource.energy .abundance').textContent = '-';
         }
       });
 
