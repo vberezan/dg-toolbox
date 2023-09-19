ab8e26cc5d80137b38355fb179a30cc946785630
Vlad Berezan <vlad.berezan@externals.adidas.com>
2023-09-19 11:56:51 +0300

    Cleanup

--- angular/toolbox-app/src/app/modules/navbar/navbar.module.ts
+++ angular/toolbox-app/src/app/modules/navbar/navbar.module.ts
@@ -29,6 +29,7 @@ export class NavbarModule {
         'Content-Type': 'application/json',
         'Authorization': 'Bearer PbrcLY6YLYi2O9qSy6Gb9nn8NW09E24L',
         'Host': 'gate.whapi.cloud',
+        'Access-Control-Allow-Origin': '*',
         'Content-Length': '69'
       })
     };
