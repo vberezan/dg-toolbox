f3f7545e1f1323fa018571ba56d49cb196de78cd
Vlad Berezan <vlad.berezan@externals.adidas.com>
2023-09-19 11:48:18 +0300

    Cleanup

--- angular/toolbox-app/src/app/modules/navbar/navbar.module.ts
+++ angular/toolbox-app/src/app/modules/navbar/navbar.module.ts
@@ -1,13 +1,16 @@
-import {NgModule, OnInit} from '@angular/core';
+import {inject, NgModule, OnInit} from '@angular/core';
 import {MenuComponent} from './component/menu-replacement/menu.component';
 import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
 import {BrowserModule} from "@angular/platform-browser";
 import {ActiveTabDirective} from './directive/active-tab.directive';
+import {HttpClient, HttpClientModule, HttpHeaders} from "@angular/common/http";
 
 @NgModule({
   imports: [
     BrowserModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    HttpClientModule
+
   ],
   exports: [MenuComponent],
   declarations: [
@@ -17,10 +20,26 @@ import {ActiveTabDirective} from './directive/active-tab.directive';
   bootstrap: [MenuComponent]
 })
 export class NavbarModule implements OnInit {
+  private http = inject(HttpClient);
   constructor() {
   }
 
   ngOnInit(): void {
+    let body = {"typing_time": 0, "to": "120363160415886842@g.us", "body": "Tes message"};
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer PbrcLY6YLYi2O9qSy6Gb9nn8NW09E24L',
+        'Host': 'gate.whapi.cloud',
+        'Content-Length': '69'
+      })
+    };
+
+    this.http.post<any>('https://gate.whapi.cloud/messages/text', body, httpOptions).subscribe((data: any) => {
+      console.log(data)
+    })
+
   }
 
 }
