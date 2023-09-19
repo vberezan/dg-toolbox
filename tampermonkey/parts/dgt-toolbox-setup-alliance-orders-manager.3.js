4bca5779efba1fde35cabd1dcabc9856f88e25dd
Vlad Berezan <vlad.berezan@externals.adidas.com>
2023-09-19 21:27:29 +0300

    Cleanup

--- tampermonkey/parts/dgt-toolbox-setup-alliance-orders-manager.3.js
+++ tampermonkey/parts/dgt-toolbox-setup-alliance-orders-manager.3.js
@@ -1,5 +1,5 @@
 function setUpAllianceOrdersManagerPanel(windowURL) {
-    if (windowURL[1] === 'planets') {
+    if (windowURL[1] === 'alliances') {
         if (document.querySelector('.allianceBox > .playerList')) {
             document.querySelector('.allianceBox > .playerList').append(document.createElement('dgt-alliance-orders-manager-panel'));
         }
