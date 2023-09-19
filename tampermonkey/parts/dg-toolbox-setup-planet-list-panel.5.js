d3fc878fd3d20fcaf71622073b5c9215a450040f
Vlad Berezan <vlad.berezan@externals.adidas.com>
2023-09-19 21:25:06 +0300

    Cleanup

--- tampermonkey/parts/dg-toolbox-setup-planet-list-panel.5.js
+++ tampermonkey/parts/dg-toolbox-setup-planet-list-panel.5.js
@@ -1,4 +1,4 @@
-function setUpPlanetListStatsPanel() {
+function setUpPlanetListStatsPanel(windowURL) {
     if (windowURL[1] === 'planets') {
         document.getElementById('planetList').prepend(document.createElement('dgt-planet-list-stats-panel'));
     }
--- tampermonkey/parts/dgt-toolbox-setup-alliance-orders-manager.1.js
+++ tampermonkey/parts/dgt-toolbox-setup-alliance-orders-manager.1.js
@@ -1,4 +1,4 @@
-function setUpAllianceOrdersManagerPanel() {
+function setUpAllianceOrdersManagerPanel(windowURL) {
     if (windowURL[1] === 'planets') {
         if (document.querySelector('.allianceBox > .playerList')) {
             document.querySelector('.allianceBox > .playerList').append(document.createElement('dgt-alliance-orders-manager-panel'));
