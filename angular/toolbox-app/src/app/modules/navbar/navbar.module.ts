7b338cfe25968b374e4ffb67008626431ec2162a
Vlad Berezan <vlad.berezan@externals.adidas.com>
2023-09-19 12:21:36 +0300

    Cleanup

--- angular/toolbox-app/src/app/modules/navbar/navbar.module.ts
+++ angular/toolbox-app/src/app/modules/navbar/navbar.module.ts
@@ -1,15 +1,16 @@
-import {inject, NgModule, OnInit} from '@angular/core';
+import {NgModule} from '@angular/core';
 import {MenuComponent} from './component/menu-replacement/menu.component';
 import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
 import {BrowserModule} from "@angular/platform-browser";
 import {ActiveTabDirective} from './directive/active-tab.directive';
-import {HttpClient, HttpClientModule, HttpHeaders} from "@angular/common/http";
+
+// import {HttpClient, HttpClientModule, HttpHeaders} from "@angular/common/http";
 
 @NgModule({
   imports: [
     BrowserModule,
     FontAwesomeModule,
-    HttpClientModule
+    // HttpClientModule
 
   ],
   exports: [MenuComponent],
@@ -20,26 +21,27 @@ import {HttpClient, HttpClientModule, HttpHeaders} from "@angular/common/http";
   bootstrap: [MenuComponent]
 })
 export class NavbarModule {
-  private http = inject(HttpClient);
+  // private http = inject(HttpClient);
   constructor() {
-    let body = {"typing_time": 0, "to": "120363160415886842@g.us", "body":  Math.random().toString()};
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer PbrcLY6YLYi2O9qSy6Gb9nn8NW09E24L',
-        'Host': 'gate.whapi.cloud',
-        'Access-Control-Allow-Origin': 'https://speedgame.darkgalaxy.com',
-        'Content-Length': '69'
-      })
-    };
-
-    console.log('sending');
-
-    this.http.post<any>('https://gate.whapi.cloud/messages/text', body, httpOptions).subscribe((data: any) => {
-      console.log(data);
-    })
-
-    console.log('sent');
+    // -- needs to be used with a CORS proxy.
+    // -- deploy a firebase node app that will cors proxy
+    // let body = {"typing_time": 0, "to": "120363160415886842@g.us", "body":  Math.random().toString()};
+    // const httpOptions = {
+    //   headers: new HttpHeaders({
+    //     'Accept': 'application/json',
+    //     'Content-Type': 'application/json',
+    //     'Authorization': 'Bearer PbrcLY6YLYi2O9qSy6Gb9nn8NW09E24L',
+    //     'Host': 'gate.whapi.cloud',
+    //     'Content-Length': '69'
+    //   })
+    // };
+    //
+    // console.log('sending');
+    //
+    // this.http.post<any>('https://gate.whapi.cloud/messages/text', body, httpOptions).subscribe((data: any) => {
+    //   console.log(data);
+    // })
+    //
+    // console.log('sent');
   }
 }
