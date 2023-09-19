e65919b5e64ef791d7f5275b74f89c0eec49aa2d
Vlad Berezan <vlad.berezan@externals.adidas.com>
2023-09-19 20:49:31 +0300

    Cleanup

--- angular/toolbox-app/src/app/modules/planets/planet-list-stats/component/stats-panel/stats-panel.component.ts
+++ angular/toolbox-app/src/app/modules/planets/planet-list-stats/component/stats-panel/stats-panel.component.ts
@@ -2,15 +2,12 @@ import {Component, inject, OnInit} from '@angular/core';
 import {StatsPanel} from "../../../../../model/planet-list/planet-list-stats-panel.model";
 import {StatsPanelService} from "../../service/stats-panel.service";
 import {FaIconLibrary} from "@fortawesome/angular-fontawesome";
-import {
-  faBolt as fasBolt,
-  faCarrot as fasCarrot,
-  faCoins as fasCoins,
-  faCubesStacked as fasCubesStacked,
-  faPersonRifle as fasPersonRifle,
-  faSatellite as fasSatellite,
-  faTreeCity as fasTreeCity
-} from "@fortawesome/free-solid-svg-icons";
+// import {
+//   faBolt as fasBolt,
+//   faCarrot as fasCarrot,
+//   faCoins as fasCoins,
+//   faCubesStacked as fasCubesStacked,
+// } from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: 'dgt-planet-list-stats-panel',
@@ -21,9 +18,9 @@ export class StatsPanelComponent implements OnInit {
   protected panel: StatsPanel;
   private statsPanelService: StatsPanelService = inject(StatsPanelService);
 
-  constructor(library: FaIconLibrary) {
-    library.addIcons(fasCubesStacked, fasCarrot, fasCoins, fasBolt, fasSatellite, fasTreeCity, fasPersonRifle);
-  }
+  // constructor(library: FaIconLibrary) {
+  // library.addIcons(fasCubesStacked, fasCarrot, fasCoins, fasBolt, fasSatellite, fasTreeCity, fasPersonRifle);
+  // }
 
   ngOnInit() {
     this.panel = this.statsPanelService.extractStats();
