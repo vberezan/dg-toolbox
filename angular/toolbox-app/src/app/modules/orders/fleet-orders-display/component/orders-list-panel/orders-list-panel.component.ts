2994e1d3770026931309ddc2c9515e41b892c6ce
Vlad Berezan <vlad.berezan@externals.adidas.com>
2023-09-20 23:22:48 +0300

    orders

--- angular/toolbox-app/src/app/modules/orders/fleet-orders-display/component/orders-list-panel/orders-list-panel.component.ts
+++ angular/toolbox-app/src/app/modules/orders/fleet-orders-display/component/orders-list-panel/orders-list-panel.component.ts
@@ -2,6 +2,7 @@ import {Component, inject, OnInit} from '@angular/core';
 import {OrderService} from "../../service/order.service";
 import {DarkgalaxyApiService} from "../../../../darkgalaxy-ui-parser/service/darkgalaxy-api.service";
 import {AllianceOrder} from "../../../../../model/orders/alliance-order.model";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'dgt-fleet-orders-list-panel',
@@ -11,11 +12,11 @@ import {AllianceOrder} from "../../../../../model/orders/alliance-order.model";
 export class OrdersListPanelComponent implements OnInit {
   private orderService: OrderService = inject(OrderService);
   private dgAPI: DarkgalaxyApiService = inject(DarkgalaxyApiService);
-  protected orders: Promise<AllianceOrder[]>;
+  protected orders: Observable<AllianceOrder[]>;
 
   constructor() {
-    this.orders = new Promise<AllianceOrder[]>((resolve) => {
-      this.orderService.getOrders(this.dgAPI.getUser(), resolve);
+    this.orders = new Observable<AllianceOrder[]>((subscriber) => {
+      this.orderService.getOrders(this.dgAPI.getUser(), subscriber.next);
     });
   }
 
