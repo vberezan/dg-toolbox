af49d1be46d1a3945457c81bcdf3414b08469c09
Vlad Berezan <vlad.berezan@externals.adidas.com>
2023-09-21 23:54:26 +0300

    orders

--- angular/toolbox-app/src/app/shared/directive/navigation-target.directive.ts
+++ angular/toolbox-app/src/app/shared/directive/navigation-target.directive.ts
@@ -4,9 +4,7 @@ import {Directive, ElementRef, HostListener} from '@angular/core';
   selector: '[dgtNavigationTarget]'
 })
 export class NavigationTargetDirective {
-  regex: RegExp = /^(?:[1-9]|[1-4]\d)\.(?:(?:[1-6]|1\d|2[0-5])|(?:[1-9]|1[0-2]))\.[1-4]\.(?:[1-9]|[1-9]\d)$/;
-
-  private specialKeys: Array<string> = ['Backspace', 'Space', 'Tab', 'End', 'Home'];
+  regex: RegExp = /^(?:(?:1\.(?:[1-9]|1\d|2[0-5]))|(?:[2-9]|1[0-3])\.(?:[1-6])|(?:[1-4][0-9]|49)\.(?:[1-2]))\.(?:[1-4](?:\.(?:[1-9]|1[0-2]))?|[1-9])$|^2\.(?:[1-6])\.(?:[1-4](?:\.(?:[1-6]))?|[1-9])$|^(?:([3-9]|1[0-3])\.(?:[1-6])\.(?:[1-4](?:\.(?:[1-9]|1[0-2]))?|[1-9]))$|^([1-4][0-9]|49)\.(?:[1-2])\.(?:[1-4](?:\.(?:[1-9]|1[0-9]))?|[1-9])$/;
 
 
   constructor(private el: ElementRef) {}
@@ -14,10 +12,6 @@ export class NavigationTargetDirective {
   @HostListener('keydown', ['$event'])
   onKeyDown(event: KeyboardEvent) {
     console.log(event.key);
-
-    if (this.specialKeys.indexOf(event.key) !== -1) {
-      return;
-    }
 
     let current: string = this.el.nativeElement.value;
     let next: string = current.concat(event.key);
