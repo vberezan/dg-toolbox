141f8d9912e07896e5ee8b4c37ff1f8b1d3b1090
Vlad Berezan <vlad.berezan@externals.adidas.com>
2023-09-21 14:25:03 +0300

    orders

--- angular/toolbox-app/src/app/modules/orders/alliance-orders-manager/component/orders-panel/orders-panel.component.ts
+++ angular/toolbox-app/src/app/modules/orders/alliance-orders-manager/component/orders-panel/orders-panel.component.ts
@@ -40,15 +40,15 @@ export class OrdersPanelComponent implements OnInit, OnDestroy {
     ngOnInit() {
         this.authService.authState.subscribe((state: AuthState) => {
             if (state.status && state.role == UserRole.ADMIN) {
-                document.querySelectorAll('.allianceBox .playerList table').forEach((element: any) => {
-                    element.style.display = '';
+                document.querySelectorAll('.allianceBox .playerList table').forEach((table: any) => {
+                    table.style.display = 'table';
                 });
                 document.querySelectorAll('.allianceBox .playerList div.name').forEach((playerName: any, idx: number) => {
                     this.orderService.fillActiveOrders(playerName.childNodes[0].textContent.trim(), this.dgAPI.gameTurn(), idx);
                 });
             } else {
-                document.querySelectorAll('.allianceBox .playerList table').forEach((element: Element) => {
-                    element.remove();
+                document.querySelectorAll('.allianceBox .playerList table').forEach((table: Element) => {
+                    table.remove();
                 });
             }
         });
