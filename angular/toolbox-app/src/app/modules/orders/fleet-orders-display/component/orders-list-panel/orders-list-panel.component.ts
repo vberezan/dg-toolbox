352ac2ada9e199f312406dcbfbb358ad138e4d10
Vlad Berezan <vlad.berezan@externals.adidas.com>
2023-09-21 11:01:52 +0300

    orders

--- angular/toolbox-app/src/app/modules/orders/fleet-orders-display/component/orders-list-panel/orders-list-panel.component.ts
+++ angular/toolbox-app/src/app/modules/orders/fleet-orders-display/component/orders-list-panel/orders-list-panel.component.ts
@@ -14,34 +14,16 @@ export class OrdersListPanelComponent {
   private orderService: OrderService = inject(OrderService);
   private dgAPI: DarkgalaxyApiService = inject(DarkgalaxyApiService);
   private changeDetection: ChangeDetectorRef = inject(ChangeDetectorRef);
-  private authService: AuthService = inject(AuthService);
+  public authService: AuthService = inject(AuthService);
 
   public orders: Observable<AllianceOrder[]>;
-  public active: Observable<boolean>;
-  observerx: Subscriber<boolean>;
 
   constructor() {
-
-
-    this.active = new Observable<boolean>((observer: Subscriber<boolean>) => {
-      this.observerx = observer;
-      observer.next(false);
-    });
-
-    this.active.subscribe((val) => {
-      console.log('subs: ' + val);
-    });
-
     this.authService.loggedStatus.subscribe((status: boolean) => {
       if (status) {
-        this.observerx.next(true);
-        console.log(true);
-
         this.orders = new Observable<AllianceOrder[]>((observer: Subscriber<AllianceOrder[]>) => {
           this.orderService.getOrders(this.dgAPI.username(), this.dgAPI.gameTurn(), observer, this.changeDetection);
         });
-      } else {
-        this.observerx.next(false);
       }
     });
   }
