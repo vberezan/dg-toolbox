82f8407c76b18b17a7b19f6cf36559ca181a71c1
Vlad Berezan <vlad.berezan@externals.adidas.com>
2023-09-21 09:18:43 +0300

    orders

--- angular/toolbox-app/src/app/modules/orders/fleet-orders-display/component/orders-list-panel/orders-list-panel.component.ts
+++ angular/toolbox-app/src/app/modules/orders/fleet-orders-display/component/orders-list-panel/orders-list-panel.component.ts
@@ -1,36 +1,23 @@
-import {ChangeDetectorRef, Component, inject, OnDestroy, OnInit} from '@angular/core';
+import {ChangeDetectorRef, Component, inject} from '@angular/core';
 import {OrderService} from "../../service/order.service";
 import {DarkgalaxyApiService} from "../../../../darkgalaxy-ui-parser/service/darkgalaxy-api.service";
 import {AllianceOrder} from "../../../../../model/orders/alliance-order.model";
-import {Observable, Subscriber, Subscription} from "rxjs";
+import {Observable, Subscriber} from "rxjs";
 
 @Component({
   selector: 'dgt-fleet-orders-list-panel',
   templateUrl: './orders-list-panel.component.html',
   styleUrls: ['./orders-list-panel.component.css']
 })
-export class OrdersListPanelComponent implements OnInit, OnDestroy {
+export class OrdersListPanelComponent {
   private orderService: OrderService = inject(OrderService);
   private dgAPI: DarkgalaxyApiService = inject(DarkgalaxyApiService);
   private changeDetection: ChangeDetectorRef = inject(ChangeDetectorRef);
   public orders: Observable<AllianceOrder[]>;
-  public hideComponent: boolean = true;
-  private ordersSubscription: Subscription;
 
   constructor() {
     this.orders = new Observable<AllianceOrder[]>((observer: Subscriber<AllianceOrder[]>) => {
       this.orderService.getOrders(this.dgAPI.getUser(), observer, this.changeDetection);
     });
-
-    this.ordersSubscription = this.orders.subscribe((orders) => {
-      this.hideComponent = (orders.length == 0);
-    });
-  }
-
-  ngOnInit(): void {
-  }
-
-  ngOnDestroy(): void {
-    this.ordersSubscription.unsubscribe();
   }
 }
