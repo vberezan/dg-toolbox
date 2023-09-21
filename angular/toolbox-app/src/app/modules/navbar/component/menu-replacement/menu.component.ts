89a672558f96b6f077ff4f4784b4b0d61582476f
Vlad Berezan <vlad.berezan@externals.adidas.com>
2023-09-21 13:25:31 +0300

    orders

--- angular/toolbox-app/src/app/modules/navbar/component/menu-replacement/menu.component.ts
+++ angular/toolbox-app/src/app/modules/navbar/component/menu-replacement/menu.component.ts
@@ -10,8 +10,10 @@ import {
   faSatelliteDish as fasSatelliteDish
 } from "@fortawesome/free-solid-svg-icons";
 import {BadgeService} from "../../service/badge.service";
-import {Observable} from "rxjs";
+import {Observable, Subscriber} from "rxjs";
 import {DarkgalaxyApiService} from "../../../darkgalaxy-ui-parser/service/darkgalaxy-api.service";
+import {AuthService} from "../../../authentication/service/auth.service";
+import {AllianceOrder} from "../../../../model/orders/alliance-order.model";
 
 @Component({
   selector: 'dgt-navbar',
@@ -23,13 +25,24 @@ export class MenuComponent implements AfterViewInit {
   private badgeService: BadgeService = inject(BadgeService);
   private dgAPI: DarkgalaxyApiService = inject(DarkgalaxyApiService);
   private changeDetection: ChangeDetectorRef = inject(ChangeDetectorRef);
+  private authService: AuthService = inject(AuthService);
+
   public fleetOrdersNotification: Observable<number>;
+  public active: boolean;
 
   constructor(library: FaIconLibrary) {
     library.addIcons(fasHouseChimney, fasEarthAmericas, fasSatelliteDish, fasJetFighterUp, fasChessBoard, fasFlaskVial, fasHandFist);
 
-    this.fleetOrdersNotification = new Observable<number>((observer) => {
-      this.badgeService.subscribeToFleetOrders(this.dgAPI.username(), observer, this.changeDetection);
+    this.authService.loggedStatus.subscribe((status: boolean) => {
+      this.active = status;
+
+      if (status) {
+        this.fleetOrdersNotification = new Observable<number>((observer) => {
+          this.badgeService.subscribeToFleetOrders(this.dgAPI.username(), observer, this.changeDetection);
+        });
+      }
+
+      this.changeDetection.detectChanges();
     });
   }
 
