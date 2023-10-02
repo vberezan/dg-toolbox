b94755c62a0eaefa3a2941d51d0b3ce80444cc08
Vlad Berezan <vlad.berezan@externals.adidas.com>
2023-10-02 11:48:29 +0300

    orders

--- angular/toolbox-app/src/app/shared/directive/navigation-target.directive.ts
+++ angular/toolbox-app/src/app/shared/directive/navigation-target.directive.ts
@@ -5,6 +5,7 @@ import {Directive, ElementRef, HostListener} from '@angular/core';
 })
 export class NavigationTargetDirective {
   private specialKeys: Array<string> = ['Backspace', 'Tab', 'End', 'Home', 'Delete', 'Del', 'ArrowLeft', 'ArrowRight', 'Left', 'Right'];
+  private regex: RegExp = new RegExp(/^[0-9.]+$/);
 
   constructor(private el: ElementRef) {
   }
@@ -12,35 +13,31 @@ export class NavigationTargetDirective {
   // -- validate that input contains only numbers and point
   @HostListener('keypress', ['$event'])
   validateInput(event: KeyboardEvent): void {
-    let copyPaste = false;
-    if (((event.ctrlKey && event.key === 'c') || (event.ctrlKey && event.key === 'v')) ||
-      ((event.metaKey && event.key === 'v') || (event.metaKey && event.key === 'c'))) {
-      copyPaste = true;
+    // -- CTRL/CMD/SHIFT/ALT
+    if (event.ctrlKey || event.metaKey || event.shiftKey || event.altKey) {
+      return;
     }
 
-    if (!copyPaste) {
-      // -- CTRL/CMD/SHIFT/ALT
-      if (event.ctrlKey || event.metaKey || event.shiftKey || event.altKey) {
-        return;
-      }
-
-      // -- special keys
-      if (this.specialKeys.indexOf(event.key) !== -1) {
-        return;
-      }
+    // -- special keys
+    if (this.specialKeys.indexOf(event.key) !== -1) {
+      return;
     }
 
-    const regex: RegExp = new RegExp(/^[0-9.]+$/);
 
     let current: string = this.el.nativeElement.value + event.key;
 
     console.log(current);
-    console.log(regex.test(current));
+    console.log(this.regex.test(current));
 
-    if (!regex.test(current)) {
+    if (!this.regex.test(current)) {
       event.preventDefault()
     }
   }
 
-
+  @HostListener('paste', ['$event'])
+  validatePaste(event: ClipboardEvent): void {
+    if (!this.regex.test(event.clipboardData.getData('text'))) {
+      event.preventDefault()
+    }
+  }
 }
