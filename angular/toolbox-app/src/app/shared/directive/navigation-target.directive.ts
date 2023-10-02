7e49f6d646e4565c98313deb733d9ffc34061726
Vlad Berezan <vlad.berezan@externals.adidas.com>
2023-10-02 11:35:30 +0300

    orders

--- angular/toolbox-app/src/app/shared/directive/navigation-target.directive.ts
+++ angular/toolbox-app/src/app/shared/directive/navigation-target.directive.ts
@@ -4,13 +4,24 @@ import {Directive, ElementRef, HostListener} from '@angular/core';
   selector: '[dgtNavigationTarget]'
 })
 export class NavigationTargetDirective {
+  private specialKeys: Array<string> = ['Backspace', 'Tab', 'End', 'Home', 'Delete', 'Del', 'ArrowLeft', 'ArrowRight', 'Left', 'Right'];
 
-
-  constructor(private el: ElementRef) {}
+  constructor(private el: ElementRef) {
+  }
 
   // -- validate that input contains only numbers and point
   @HostListener('keydown', ['$event'])
   validateInput(event: KeyboardEvent): void {
+    // -- CTRL/CMD/SHIFT/ALT
+    if (event.ctrlKey || event.metaKey || event.shiftKey || event.altKey) {
+      return;
+    }
+
+    // -- special keys
+    if (this.specialKeys.indexOf(event.key) !== -1) {
+      return;
+    }
+
     const regex: RegExp = new RegExp(/^[0-9.]+$/);
 
     let current: string = this.el.nativeElement.value + event.key;
