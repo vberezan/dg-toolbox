0e8b148216d47017562907d880fe64abbd621fcc
Vlad Berezan <vlad.berezan@externals.adidas.com>
2023-10-04 22:46:04 +0300

    orders

--- angular/toolbox-app/src/app/modules/darkgalaxy-ui-parser/service/alliance-members.service.ts
+++ angular/toolbox-app/src/app/modules/darkgalaxy-ui-parser/service/alliance-members.service.ts
@@ -11,9 +11,9 @@ export class AllianceMembersService implements DataExtractor {
   private localStorageService: LocalStorageService = inject(LocalStorageService);
 
   extract(): AllianceMember[] {
-    let local: AllianceMember[] = Object.assign([], this.localStorageService.getWithExpiry(LocalStorageKeys.ALLIANCE_MEMBERS));
+    let local: AllianceMember[] = this.localStorageService.getWithExpiry(LocalStorageKeys.ALLIANCE_MEMBERS);
 
-    if (local.length > 0) {
+    if (local != null) {
       return local;
     }
 
