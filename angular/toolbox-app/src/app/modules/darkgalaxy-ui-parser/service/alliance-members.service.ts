85f30ef2c7e5b94155f529246dee5656315a8ab0
Vlad Berezan <vlad.berezan@externals.adidas.com>
2023-10-05 17:19:56 +0300

    orders

--- angular/toolbox-app/src/app/modules/darkgalaxy-ui-parser/service/alliance-members.service.ts
+++ angular/toolbox-app/src/app/modules/darkgalaxy-ui-parser/service/alliance-members.service.ts
@@ -55,10 +55,10 @@ export class AllianceMembersService implements DataExtractor {
       document.querySelector('.dgt-orders-manager-panel.user').remove();
     }
 
-    document.querySelectorAll('.allianceBox').forEach((allianceBox: any): void => {
-      if (allianceBox.querySelector('.plainHeader').childNodes[0].textContent.trim().toLowerCase() === 'member list') {
-        allianceBox.querySelector('.plainHeader').remove();
-        allianceBox.querySelector('.playerList').remove();
+    document.querySelectorAll('.allianceBox > .playerList').forEach((list: any): void => {
+      if (list.parentElement.querySelector('.plainHeader').childNodes[0].textContent.trim().toLowerCase() === 'member list') {
+        list.parentNode.querySelector('.plainHeader').remove();
+        list.remove();
       }
     });
   }
