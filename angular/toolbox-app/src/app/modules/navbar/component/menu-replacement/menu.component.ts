185453aea217a3e3c470bb7d5966c32f241aeeaf
Vlad Berezan <vlad.berezan@externals.adidas.com>
2023-10-06 10:12:20 +0300

    orders

--- angular/toolbox-app/src/app/modules/navbar/component/menu-replacement/menu.component.ts
+++ angular/toolbox-app/src/app/modules/navbar/component/menu-replacement/menu.component.ts
@@ -14,6 +14,8 @@ import {Observable, Subscriber} from "rxjs";
 import {DarkgalaxyApiService} from "../../../darkgalaxy-ui-parser/service/darkgalaxy-api.service";
 import {AuthService} from "../../../authentication/service/auth.service";
 import {AuthState} from "../../../../shared/model/authentication/auth-state.model";
+import {LocalStorageService} from "../../../local-storage-manager/service/local-storage.service";
+import {LocalStorageKeys} from "../../../../shared/model/local-storage/local-storage-keys";
 
 @Component({
   selector: 'dgt-navbar',
@@ -26,12 +28,15 @@ export class MenuComponent implements AfterViewInit, OnDestroy {
   private dgAPI: DarkgalaxyApiService = inject(DarkgalaxyApiService);
   private changeDetection: ChangeDetectorRef = inject(ChangeDetectorRef);
   private authService: AuthService = inject(AuthService);
+  private localStorageService: LocalStorageService = inject(LocalStorageService);
+  protected localOrdersBadge: number = 0;
 
   public fleetOrdersNotification: Observable<number>;
   public active: boolean;
 
   constructor(library: FaIconLibrary) {
     library.addIcons(fasHouseChimney, fasEarthAmericas, fasSatelliteDish, fasJetFighterUp, fasChessBoard, fasFlaskVial, fasHandFist);
+    this.localOrdersBadge = this.localStorageService.get(LocalStorageKeys.ACTIVE_ORDERS);
 
     this.authService.authState.subscribe((state: AuthState): void => {
       this.active = state.status;
--- angular/toolbox-app/src/app/modules/navbar/service/badge.service.ts
+++ angular/toolbox-app/src/app/modules/navbar/service/badge.service.ts
@@ -3,12 +3,15 @@ import {collection, collectionData, Firestore, query, where} from "@angular/fire
 import {Subscriber} from "rxjs";
 import firebase from "firebase/compat";
 import DocumentData = firebase.firestore.DocumentData;
+import {LocalStorageService} from "../../local-storage-manager/service/local-storage.service";
+import {LocalStorageKeys} from "../../../shared/model/local-storage/local-storage-keys";
 
 @Injectable({
   providedIn: 'root'
 })
 export class BadgeService {
   private firestore: Firestore = inject(Firestore);
+  private localStorageService: LocalStorageService = inject(LocalStorageService);
 
   subscribeToFleetOrders(user: string, observer: Subscriber<number>, changeDetection: ChangeDetectorRef) {
     let ordersRef = collection(this.firestore, 'orders');
@@ -20,6 +23,12 @@ export class BadgeService {
       ), {idField: 'id'}
     ).forEach((items: DocumentData[]) => {
       observer.next(items.length);
+      if (items.length > 0 ) {
+        this.localStorageService.cache(LocalStorageKeys.ACTIVE_ORDERS, items.length);
+      } else {
+        this.localStorageService.remove(LocalStorageKeys.ACTIVE_ORDERS);
+      }
+
       changeDetection.detectChanges();
     }).catch((error) => {
       console.log(error);
