1adf9833031a2f94a0660212daab8edd6ff9bef7
Vlad Berezan <vlad.berezan@externals.adidas.com>
2023-10-07 00:50:09 +0300

    orders

--- tampermonkey/parts/dg-toolbox-replace-ships-images.3.js
+++ tampermonkey/parts/dg-toolbox-replace-ships-images.3.js
@@ -1,8 +1,8 @@
 function replaceShipsImages() {
     replaceImgWithImg('/images/units/research/merchant.png', 'https://i.imgur.com/ZmWLauZ.png');
-    replaceImgWithImg('/images/units/research/destroyer.png', 'https://i.imgur.com/3lsCm9N.png');
+    replaceImgWithImg('/images/units/research/destroyer.png', 'https://i.imgur.com/LSqmfvq.png');
     replaceImgWithImg('/images/units/research/trader.png', 'https://i.imgur.com/JqkqHc5.png');
-    replaceImgWithImg('/images/units/research/cruiser.png', 'https://i.imgur.com/JsmYFFS.png');
+    replaceImgWithImg('/images/units/research/cruiser.png', 'https://i.imgur.com/ACYnTKC.png');
     replaceImgWithImg('/images/units/research/hulk.png', 'https://i.imgur.com/cxCkxsk.png');
-    replaceImgWithImg('/images/units/research/battleship.png', 'https://i.imgur.com/Pv2XL86.png');
+    replaceImgWithImg('/images/units/research/battleship.png', 'https://i.imgur.com/9Kgt693.png');
 }
