86207269a3637d837ebd3bd7f09f69b850530992
Vlad Berezan <vlad.berezan@externals.adidas.com>
2023-10-08 15:57:37 +0300

    orders

--- tampermonkey/parts/dg-toolbox-replace-icons-with-fa-icons.8.js
+++ tampermonkey/parts/dg-toolbox-replace-icons-with-fa-icons.8.js
@@ -10,11 +10,6 @@ function replaceIconsWithFAIcons() {
     );
 
     replaceImgWithFontAwsome(
-        '/images/icons/mail.png',
-        '<fa-icon class="ng-fa-icon mail" style="color: rgb(223, 74, 74);"><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M64 112c-8.8 0-16 7.2-16 16v22.1L220.5 291.7c20.7 17 50.4 17 71.1 0L464 150.1V128c0-8.8-7.2-16-16-16H64zM48 212.2V384c0 8.8 7.2 16 16 16H448c8.8 0 16-7.2 16-16V212.2L322 328.8c-38.4 31.5-93.7 31.5-132 0L48 212.2zM0 128C0 92.7 28.7 64 64 64H448c35.3 0 64 28.7 64 64V384c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V128z"/></svg></fa-icon>'
-    );
-
-    replaceImgWithFontAwsome(
         '/images/icons/logout.png',
         '<fa-icon class="ng-fa-icon logout" style="color: rgb(223, 74, 74);"><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M502.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L402.7 224 192 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l210.7 0-73.4 73.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l128-128zM160 96c17.7 0 32-14.3 32-32s-14.3-32-32-32L96 32C43 32 0 75 0 128L0 384c0 53 43 96 96 96l64 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-64 0c-17.7 0-32-14.3-32-32l0-256c0-17.7 14.3-32 32-32l64 0z"/></svg></fa-icon>'
     );
