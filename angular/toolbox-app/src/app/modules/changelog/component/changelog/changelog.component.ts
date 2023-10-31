7e5285a81d7c79bdf496a0e81191007e6671b783
Vlad Berezan <vlad.berezan@externals.adidas.com>
2023-10-31 15:34:13 +0200

    changelog

--- angular/toolbox-app/src/app/modules/changelog/component/changelog/changelog.component.ts
+++ angular/toolbox-app/src/app/modules/changelog/component/changelog/changelog.component.ts
@@ -1,6 +1,7 @@
 import {ChangeDetectorRef, Component, inject} from '@angular/core';
 import {ChangelogService} from "../../service/changelog.service";
 import {Observable, Subscriber} from "rxjs";
+import {LocalStorageService} from "../../../local-storage-manager/service/local-storage.service";
 
 @Component({
   selector: 'dgt-changelog',
@@ -10,12 +11,16 @@ import {Observable, Subscriber} from "rxjs";
 export class ChangelogComponent {
   private changeLogService: ChangelogService = inject(ChangelogService);
   private changeDetection: ChangeDetectorRef = inject(ChangeDetectorRef);
+  private localStorageService: LocalStorageService = inject(LocalStorageService);
 
   public changed: Observable<boolean>;
+  public version: string;
 
   constructor() {
     this.changed = new Observable<boolean>((observer: Subscriber<boolean>): void => {
       this.changeLogService.checkVersion(this.changeDetection, observer);
     });
+
+    this.version = this.localStorageService.getVersion();
   }
 }
