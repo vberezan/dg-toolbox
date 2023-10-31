06123e0ecedffbf7f469dcb77bc6092977375dde
Vlad Berezan <vlad.berezan@externals.adidas.com>
2023-10-31 11:00:54 +0200

    orders

--- angular/toolbox-app/src/app/modules/changelog/changelog.module.ts
+++ angular/toolbox-app/src/app/modules/changelog/changelog.module.ts
@@ -2,11 +2,23 @@ import {NgModule} from '@angular/core';
 import {ChangelogComponent} from './component/changelog/changelog.component';
 import {BrowserModule} from "@angular/platform-browser";
 import {ChangelogService} from "./service/changelog.service";
+import {getApp, initializeApp, provideFirebaseApp} from "@angular/fire/app";
+import {environment} from "../../../environments/environment";
+import {getFirestore, provideFirestore} from "@angular/fire/firestore";
+import {initializeAppCheck, provideAppCheck, ReCaptchaV3Provider} from "@angular/fire/app-check";
 
 
 @NgModule({
   imports: [
-    BrowserModule
+    BrowserModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
+    provideAppCheck(() => initializeAppCheck(getApp(),
+      {
+        provider: new ReCaptchaV3Provider(environment.firebase.appCheck.recaptchaSiteKey),
+        isTokenAutoRefreshEnabled: true
+      })
+    )
   ],
   declarations: [
     ChangelogComponent
