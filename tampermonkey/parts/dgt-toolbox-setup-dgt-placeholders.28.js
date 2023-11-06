26eb5180f8750e39f9f4e9a659ff7613b5102e62
Vlad Berezan <vlad.berezan@externals.adidas.com>
2023-11-06 14:35:37 +0200

    changelog

--- tampermonkey/parts/dgt-toolbox-setup-dgt-placeholders.28.js
+++ tampermonkey/parts/dgt-toolbox-setup-dgt-placeholders.28.js
@@ -67,7 +67,7 @@ function setUpFleetOrdersListPanel(windowURL) {
 
 function setUpSharedScansCollector(windowURL) {
     if (windowURL[1] === 'planet' && (windowURL.length === 5 && windowURL[3]) === 'comms') {
-        document.querySelector('.opacDarkBackground form').parentElement.append(document.createElement('dgt-shared-scans-collector'));
+        document.querySelector('.opacDarkBackground form').append(document.createElement('dgt-shared-scans-collector'));
     }
 }
 
