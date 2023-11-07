befead921fa3ed22e3510269fb07516e30df4c19
Vlad Berezan <vlad.berezan@externals.adidas.com>
2023-11-08 00:26:18 +0200

    changelog

--- angular/toolbox-app/src/app/modules/authentication/service/auth.service.ts
+++ angular/toolbox-app/src/app/modules/authentication/service/auth.service.ts
@@ -35,7 +35,7 @@ export class AuthService implements OnDestroy {
     let timeToken = this.localStorageService.get(LocalStorageKeys.USER).session.timeToken;
     let refreshToken = this.localStorageService.get(LocalStorageKeys.USER).session.refreshToken;
     let timestamp: number = Date.parse(CryptoJS.AES.decrypt(timeToken, refreshToken).toString(CryptoJS.enc.Utf8));
-    let status: boolean = (Date.now() - timestamp) <= 86_400_000;
+    let status: boolean = (Date.now() - timestamp) <= 604_800_000; // -- 1 week
 
     if (status && sendEvent) {
       this._authState.emit(new AuthState(status, this.localStorageService.get(LocalStorageKeys.USER).session.role));
