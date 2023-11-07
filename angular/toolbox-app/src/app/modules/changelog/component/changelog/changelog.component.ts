688f8e21ecb0ee42ab622b24ad79f6d8b92129c3
Vlad Berezan <vlad.berezan@externals.adidas.com>
2023-11-07 20:57:25 +0200

    changelog

--- angular/toolbox-app/src/app/modules/changelog/component/changelog/changelog.component.ts
+++ angular/toolbox-app/src/app/modules/changelog/component/changelog/changelog.component.ts
@@ -2,6 +2,7 @@ import {ChangeDetectorRef, Component, inject} from '@angular/core';
 import {ChangelogService} from "../../service/changelog.service";
 import {Observable, Subscriber} from "rxjs";
 import {LocalStorageService} from "../../../local-storage-manager/service/local-storage.service";
+import {LocalStorageKeys} from "../../../../shared/model/local-storage/local-storage-keys";
 
 @Component({
   selector: 'dgt-changelog',
@@ -22,5 +23,9 @@ export class ChangelogComponent {
     });
 
     this.version = this.localStorageService.getVersion();
+  }
+
+  installUpdate() {
+    this.localStorageService.cache(LocalStorageKeys.UPDATE_AVAILABLE, false);
   }
 }
