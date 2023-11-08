824eb89f5306b2f33eb2fe37599b8fbd19ee751a
Vlad Berezan <vlad.berezan@externals.adidas.com>
2023-11-09 00:49:15 +0200

    changelog

--- angular/toolbox-app/src/app/modules/admin/load-data/service/navigation-matrix.service.ts
+++ angular/toolbox-app/src/app/modules/admin/load-data/service/navigation-matrix.service.ts
@@ -61,6 +61,59 @@ export class NavigationMatrixService {
   async extractData(galaxy: number, sector: number, system: number): Promise<void> {
     let source:string = await firstValueFrom(this.httpClient.get('https://andromeda.darkgalaxy.com/navigation/' + galaxy + '/' + sector + '/' + system, {responseType: 'text'}));
 
-    console.log(source.split('<span style="font-style: normal; color: #fff">'));
+
+    console.log(JSON.stringify(this.converter(document.createElement(source)), null, 4));
   }
+
+
+  private converter(dom: any): any {
+    if (dom.nodeType === Node.TEXT_NODE) {
+      //  add only if value is not empty
+      if(dom.nodeValue && dom.nodeValue.trim() != '')
+        return dom.nodeValue;
+    }
+    if (dom.nodeType === Node.DOCUMENT_NODE) {
+      dom = dom.documentElement;
+    }
+
+    const obj: any = {};
+
+    //  add only if value is not empty
+    if(dom.nodeValue && dom.nodeValue.trim() != ''){
+      obj.nodeType = dom.nodeType;
+    }
+
+    if (dom.nodeType === Node.ELEMENT_NODE) {
+      obj.tagName = dom.tagName;
+
+      obj.attributes = []; // Array.from(obj.attributes) gives us a lot of things we don't want
+      for (let i = 0, len = dom.attributes.length; i < len; ++i) {
+        const attr = dom.attributes[i];
+        obj.attributes.push({name: attr.name, value: attr.value});
+      }
+      //  remove attributes if is empty
+      if(obj.attributes.length == 0) delete obj.attributes;
+
+      obj.children = [];
+      for (let child = dom.firstChild;child;child = child.nextSibling) {
+        //  add only if value is not NULL
+        var childVal = this.converter(child);
+        if(childVal)
+          obj.children.push(childVal);
+      }
+      //  remove children if is empty
+      if(obj.children.length == 0) delete obj.children;
+
+    } else {
+      //  add only if value is not empty
+      if(dom.nodeValue && dom.nodeValue.trim() != '')
+        obj.nodeValue = dom.nodeValue;
+    }
+
+    if(obj && Object.keys(obj).length === 0 && Object.getPrototypeOf(obj) === Object.prototype){
+      //  do nothing
+    }else
+      return obj;
+  }
+
 }
