6a8270e0d5c6cae428705f374de9dffbdb73aefe
Vlad Berezan <vlad.berezan@externals.adidas.com>
2023-11-09 00:14:49 +0200

    changelog

--- angular/toolbox-app/src/app/modules/admin/load-data/component/admin-panel/admin-panel.component.ts
+++ angular/toolbox-app/src/app/modules/admin/load-data/component/admin-panel/admin-panel.component.ts
@@ -1,6 +1,7 @@
 import {Component, inject} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {firstValueFrom} from "rxjs";
+import {NavigationMatrixService} from "../../service/navigation-matrix.service";
 
 @Component({
   selector: 'dgt-admin-load-data-panel',
@@ -9,15 +10,18 @@ import {firstValueFrom} from "rxjs";
 })
 export class AdminPanelComponent {
   private httpClient: HttpClient = inject(HttpClient);
+  private navigationMatrixService: NavigationMatrixService = inject(NavigationMatrixService);
 
   constructor() {
+    console.log(this.navigationMatrixService.generateNavigationCoordinates());
+
     this.execute().catch(reason => {
       console.log(reason);
     });
   }
 
   async execute(): Promise<void> {
-    console.log(await firstValueFrom(this.httpClient.get('https://andromeda.darkgalaxy.com/', {responseType: 'text'})));
+    // console.log(await firstValueFrom(this.httpClient.get('https://andromeda.darkgalaxy.com/', {responseType: 'text'})));
   }
 
 }
