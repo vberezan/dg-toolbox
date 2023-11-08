db96c0b90927baa3df48766f33ef4892bb78d01d
Vlad Berezan <vlad.berezan@externals.adidas.com>
2023-11-08 17:29:54 +0200

    changelog

--- tampermonkey/parts/dg-toolbox-custom-styling.87.js
+++ tampermonkey/parts/dg-toolbox-custom-styling.87.js
@@ -336,7 +336,7 @@ function applyCustomStyling() {
                 newButton.innerHTML = '<fa-icon _ngcontent-ng-c2638986096="" class="ng-fa-icon"><svg role="img" aria-hidden="true" focusable="false" data-prefix="far" data-icon="circle-right" class="svg-inline--fa fa-circle-right" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M464 256A208 208 0 1 1 48 256a208 208 0 1 1 416 0zM0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM294.6 135.1c-4.2-4.5-10.1-7.1-16.3-7.1C266 128 256 138 256 150.3V208H160c-17.7 0-32 14.3-32 32v32c0 17.7 14.3 32 32 32h96v57.7c0 12.3 10 22.3 22.3 22.3c6.2 0 12.1-2.6 16.3-7.1l99.9-107.1c3.5-3.8 5.5-8.7 5.5-13.8s-2-10.1-5.5-13.8L294.6 135.1z"></path></svg></fa-icon>';
 
                 button.parentElement.append(newButton);
-                //button.remove();
+                button.remove();
             })
         }
 
@@ -347,7 +347,7 @@ function applyCustomStyling() {
                 newButton.innerHTML = '<fa-icon _ngcontent-ng-c2638986096="" class="ng-fa-icon"><svg role="img" aria-hidden="true" focusable="false" data-prefix="far" data-icon="circle-right" class="svg-inline--fa fa-circle-right" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M464 256A208 208 0 1 1 48 256a208 208 0 1 1 416 0zM0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM294.6 135.1c-4.2-4.5-10.1-7.1-16.3-7.1C266 128 256 138 256 150.3V208H160c-17.7 0-32 14.3-32 32v32c0 17.7 14.3 32 32 32h96v57.7c0 12.3 10 22.3 22.3 22.3c6.2 0 12.1-2.6 16.3-7.1l99.9-107.1c3.5-3.8 5.5-8.7 5.5-13.8s-2-10.1-5.5-13.8L294.6 135.1z"></path></svg></fa-icon>';
 
                 button.parentElement.append(newButton);
-                //button.remove();
+                button.remove();
             })
         }
     }
