5b877147fd5041b53e6146def3bf59eb460c091e
Vlad Berezan <vlad.berezan@externals.adidas.com>
2023-11-09 10:49:31 +0200

    changelog

--- angular/toolbox-app/src/app/modules/admin/load-data/component/admin-panel/admin-panel.component.ts
+++ angular/toolbox-app/src/app/modules/admin/load-data/component/admin-panel/admin-panel.component.ts
@@ -2,20 +2,23 @@ import {Component, inject} from '@angular/core';
 import {NavigationMatrixService} from "../../service/navigation-matrix.service";
 
 @Component({
-  selector: 'dgt-admin-load-data-panel',
-  templateUrl: './admin-panel.component.html',
-  styleUrls: ['./admin-panel.component.css']
+    selector: 'dgt-admin-load-data-panel',
+    templateUrl: './admin-panel.component.html',
+    styleUrls: ['./admin-panel.component.css']
 })
 export class AdminPanelComponent {
-  private navigationMatrixService: NavigationMatrixService = inject(NavigationMatrixService);
+    private navigationMatrixService: NavigationMatrixService = inject(NavigationMatrixService);
+    protected controls: {
+        galaxies: string
+    }
 
+    async execute(galaxies: number[]): Promise<void> {
+        return this.navigationMatrixService.extractGalaxies(galaxies);
+    }
 
-  constructor() {
-    this.execute();
-  }
-
-  async execute(): Promise<void> {
-    // await this.navigationMatrixService.extractGalaxies([48, 49]);
-  }
-
+    async scanGalaxies(): Promise<void> {
+        await this.execute(this.controls.galaxies.trim().split(',').map(function (item: string) {
+            return parseInt(item, 10);
+        }));
+    }
 }
--- angular/toolbox-app/src/app/modules/admin/load-data/load-data.module.ts
+++ angular/toolbox-app/src/app/modules/admin/load-data/load-data.module.ts
@@ -3,12 +3,14 @@ import { AdminPanelComponent } from './component/admin-panel/admin-panel.compone
 import {BrowserModule} from "@angular/platform-browser";
 import { HttpClientModule} from "@angular/common/http";
 import { AllianceListDirective } from './directive/alliance-list.directive';
+import {FormsModule} from "@angular/forms";
 
 
 
 @NgModule({
   imports: [
     BrowserModule,
+    FormsModule,
     HttpClientModule
   ],
   declarations: [
