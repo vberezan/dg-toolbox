2ace7b41661a9b90987dd0617a277bd1b23ceeaf
Vlad Berezan <vlad.berezan@externals.adidas.com>
2023-11-09 12:12:29 +0200

    changelog

--- angular/toolbox-app/src/app/modules/admin/load-data/component/admin-panel/admin-panel.component.ts
+++ angular/toolbox-app/src/app/modules/admin/load-data/component/admin-panel/admin-panel.component.ts
@@ -1,5 +1,6 @@
-import {Component, inject} from '@angular/core';
+import {Component, ElementRef, inject} from '@angular/core';
 import {NavigationMatrixService} from "../../service/navigation-matrix.service";
+import {doc} from "@angular/fire/firestore";
 
 @Component({
   selector: 'dgt-admin-load-data-panel',
@@ -19,8 +20,18 @@ export class AdminPanelComponent {
   }
 
   async scanGalaxies(): Promise<void> {
+    let modal: HTMLElement = document.createElement('div');
+    modal.id = 'dgt-overlay-modal';
+
+    document.body.classList.add('.dgt-overlay-open');
+    document.body.prepend(modal);
+
+
     await this.execute(this.controls.galaxies.trim().split(',').map(function (item: string) {
       return parseInt(item, 10);
     }));
+
+    document.body.classList.remove('.dgt-overlay-open');
+    document.querySelector(modal.id).remove();
   }
 }
