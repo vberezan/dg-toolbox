97b366efe1b6f0892b6d005adad4d88d182736ad
Vlad Berezan <vlad.berezan@externals.adidas.com>
2023-11-13 16:27:30 +0200

    remove orders display feature - not useful

--- angular/toolbox-app/src/app/modules/admin/load-data/service/rankings-loader.service.ts
+++ angular/toolbox-app/src/app/modules/admin/load-data/service/rankings-loader.service.ts
@@ -175,6 +175,11 @@ export class RankingsLoaderService {
     });
 
     await this.delay(75 * playersStats.size);
+
+    updateDoc(doc(configRef, ' last-players-rankings-update-turn '), JSON.parse(JSON.stringify({'value': this.dgAPI.gameTurn()})))
+      .catch((e): void => {
+        console.log(e);
+      });
   }
 
   async scanAllianceRankingsScreens(): Promise<void> {
