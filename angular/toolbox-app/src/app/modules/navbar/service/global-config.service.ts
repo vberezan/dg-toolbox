631a3144c26d1875a7253bb9ecb97b0670839b17
Vlad Berezan <vlad.berezan@externals.adidas.com>
2023-11-13 15:16:30 +0200

    remove orders display feature - not useful

--- angular/toolbox-app/src/app/modules/navbar/service/global-config.service.ts
+++ angular/toolbox-app/src/app/modules/navbar/service/global-config.service.ts
@@ -4,7 +4,6 @@ import {LocalStorageService} from "../../local-storage-manager/service/local-sto
 import {DocumentData} from "@angular/fire/compat/firestore";
 import {LocalStorageKeys} from "../../../shared/model/local-storage/local-storage-keys";
 import {Subscription} from "rxjs";
-import {DarkgalaxyApiService} from "../../darkgalaxy-ui-parser/service/darkgalaxy-api.service";
 import {PlayerStats} from "../../../shared/model/stats/player-stats.model";
 import {PlayerStatsCache} from "../../../shared/model/stats/player-stats-cache.model";
 
@@ -13,41 +12,43 @@ import {PlayerStatsCache} from "../../../shared/model/stats/player-stats-cache.m
 })
 export class GlobalConfigService {
   private firestore: Firestore = inject(Firestore);
-  private dgAPI: DarkgalaxyApiService = inject(DarkgalaxyApiService);
   private localStorageService: LocalStorageService = inject(LocalStorageService);
 
   checkAndCachePlayersRankings(): void {
-    if (this.localStorageService.get(LocalStorageKeys.PLAYERS_STATS) &&
-      (this.localStorageService.get(LocalStorageKeys.LAST_PLAYERS_RANKINGS_UPDATE_TURN) < this.dgAPI.gameTurn())) {
+    let playerSubscription: Subscription = docData(
+      doc(collection(this.firestore, 'config'), 'last-players-rankings-update-turn')
+    ).subscribe((item: DocumentData): void => {
+      if (this.localStorageService.get(LocalStorageKeys.LAST_PLAYERS_RANKINGS_UPDATE_TURN) &&
+        (this.localStorageService.get(LocalStorageKeys.LAST_PLAYERS_RANKINGS_UPDATE_TURN) == Object.assign({value: 0}, item).value)) {
+        return;
+      } else {
+        this.localStorageService.cache(LocalStorageKeys.LAST_PLAYERS_RANKINGS_UPDATE_TURN, Object.assign({value: 0}, item).value);
 
-      let planetsSubscription: Subscription = collectionData(
-        query(collection(this.firestore, 'players'))
-      ).subscribe((items: DocumentData[]): void => {
-        let stats: PlayerStats[] = Object.assign([], items);
-        let cache: PlayerStatsCache[] = [];
+        let planetsSubscription: Subscription = collectionData(
+          query(collection(this.firestore, 'players'))
+        ).subscribe((items: DocumentData[]): void => {
+          let stats: PlayerStats[] = Object.assign([], items);
+          let cache: PlayerStatsCache[] = [];
 
-        stats.forEach((stat: PlayerStats) => {
-          let psc: PlayerStatsCache = new PlayerStatsCache();
-          psc.playerId = stat.playerId;
-          psc.rank = stat.rank;
-          psc.planets = stat.planets.length;
-          psc.name = stat.name;
-          psc.alliance = stat.alliance;
-          psc.combatScore = stat.combatScore;
-          psc.combinedScore = stat.combinedScore;
-          psc.score = stat.score;
+          stats.forEach((stat: PlayerStats) => {
+            let psc: PlayerStatsCache = new PlayerStatsCache();
+            psc.playerId = stat.playerId;
+            psc.rank = stat.rank;
+            psc.planets = stat.planets.length;
+            psc.name = stat.name;
+            psc.alliance = stat.alliance;
+            psc.combatScore = stat.combatScore;
+            psc.combinedScore = stat.combinedScore;
+            psc.score = stat.score;
+          });
+
+          this.localStorageService.cache(LocalStorageKeys.PLAYERS_STATS, cache);
+          planetsSubscription.unsubscribe();
         });
+      }
 
-        this.localStorageService.cache(LocalStorageKeys.PLAYERS_STATS, cache);
-        planetsSubscription.unsubscribe();
+      playerSubscription.unsubscribe();
 
-        let playerSubscription: Subscription = docData(
-          doc(collection(this.firestore, 'config'), 'last-players-rankings-update-turn')
-        ).subscribe((item: DocumentData): void => {
-          this.localStorageService.cache(LocalStorageKeys.LAST_PLAYERS_RANKINGS_UPDATE_TURN, Object.assign({value: 0}, item).value);
-          playerSubscription.unsubscribe();
-        });
-      });
-    }
+    });
   }
 }
