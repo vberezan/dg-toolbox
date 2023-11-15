97689b5234fa0858017ea6fa739d0525ef3aa0f7
Vlad Berezan <vlad.berezan@externals.adidas.com>
2023-11-15 22:31:56 +0200

    add local synchronizer

--- angular/toolbox-app/src/app/modules/local-storage/local-storage-synchronizer/component/local-storage-synchronizer/local-storage-synchronizer.component.ts
+++ angular/toolbox-app/src/app/modules/local-storage/local-storage-synchronizer/component/local-storage-synchronizer/local-storage-synchronizer.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {SynchronizerService} from "../../service/synchronizer.service";
 
 @Component({
   selector: 'dgt-local-storage-synchronizer',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./local-storage-synchronizer.component.css']
 })
 export class LocalStorageSynchronizerComponent {
-
+  private synchronizerService: SynchronizerService = inject(SynchronizerService);
 }
