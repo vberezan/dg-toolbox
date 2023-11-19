dd12fc1431a6006797d85cd596a1fc609bb45e40
Vlad Berezan <vlad.berezan@externals.adidas.com>
2023-11-19 18:33:41 +0200

    add local synchronizer

--- angular/toolbox-app/src/app/modules/research/research.module.ts
+++ angular/toolbox-app/src/app/modules/research/research.module.ts
@@ -3,8 +3,6 @@ import { CommonModule } from '@angular/common';
 import { ResearchPanelComponent } from './component/research-panel/research-panel.component';
 import {BrowserModule} from "@angular/platform-browser";
 
-
-
 @NgModule({
   imports: [
     BrowserModule,
