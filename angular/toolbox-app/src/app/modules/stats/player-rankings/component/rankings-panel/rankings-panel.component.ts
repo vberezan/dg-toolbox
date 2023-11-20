2321724e7d042f079ee4d333510e7ccafdc1620d
Vlad Berezan <vlad.berezan@externals.adidas.com>
2023-11-20 22:01:28 +0200

    add local synchronizer

--- angular/toolbox-app/src/app/modules/stats/player-rankings/component/rankings-panel/rankings-panel.component.ts
+++ angular/toolbox-app/src/app/modules/stats/player-rankings/component/rankings-panel/rankings-panel.component.ts
@@ -1,8 +1,6 @@
 import {Component, inject} from '@angular/core';
 import {PlayerRankingsService} from "../../service/player-rankings.service";
 import {PlayerStats} from "../../../../../shared/model/stats/player-stats.model";
-import {LocalStorageService} from "../../../../local-storage/local-storage-manager/service/local-storage.service";
-import {LocalStorageKeys} from "../../../../../shared/model/local-storage/local-storage-keys";
 
 @Component({
   selector: 'dgt-players-rankings',
@@ -11,21 +9,9 @@ import {LocalStorageKeys} from "../../../../../shared/model/local-storage/local-
 })
 export class RankingsPanelComponent {
   private playerRankingsService: PlayerRankingsService = inject(PlayerRankingsService);
-  private localStorageService: LocalStorageService = inject(LocalStorageService);
   public rankings: Map<number, PlayerStats> = new Map<number, PlayerStats>();
 
   constructor() {
     this.rankings = this.playerRankingsService.playerStats();
-    const cachedStats: PlayerStats[] = this.localStorageService.get(LocalStorageKeys.PLAYERS_STATS);
-
-    for (const player of cachedStats) {
-      const ranking: PlayerStats = this.rankings.get(player.playerId);
-      if (ranking) {
-        ranking.planets = player.planets;
-        ranking.g1449Total = player.g1449Total;
-        ranking.g213Total = player.g213Total;
-        ranking.g1Total = player.g1Total;
-      }
-    }
   }
 }
--- angular/toolbox-app/src/app/modules/stats/player-rankings/service/player-rankings.service.ts
+++ angular/toolbox-app/src/app/modules/stats/player-rankings/service/player-rankings.service.ts
@@ -1,16 +1,34 @@
 import {inject, Injectable} from '@angular/core';
 import {DarkgalaxyApiService} from "../../../darkgalaxy-ui-parser/service/darkgalaxy-api.service";
 import {PlayerStats} from "../../../../shared/model/stats/player-stats.model";
+import {LocalStorageKeys} from "../../../../shared/model/local-storage/local-storage-keys";
+import {LocalStorageService} from "../../../local-storage/local-storage-manager/service/local-storage.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlayerRankingsService {
   private dgApi: DarkgalaxyApiService = inject(DarkgalaxyApiService);
+  private localStorageService: LocalStorageService = inject(LocalStorageService);
 
   constructor() { }
 
   playerStats(): Map<number, PlayerStats> {
-    return this.dgApi.playerRankings();
+    let rankings: Map<number, PlayerStats> = this.dgApi.playerRankings();
+
+    const cachedStats: PlayerStats[] = this.localStorageService.get(LocalStorageKeys.PLAYERS_STATS);
+
+    for (const player of cachedStats) {
+      const ranking: PlayerStats = rankings.get(player.playerId);
+
+      if (ranking) {
+        ranking.planets = player.planets;
+        ranking.g1449Total = player.g1449Total;
+        ranking.g213Total = player.g213Total;
+        ranking.g1Total = player.g1Total;
+      }
+    }
+
+    return rankings;
   }
 }
