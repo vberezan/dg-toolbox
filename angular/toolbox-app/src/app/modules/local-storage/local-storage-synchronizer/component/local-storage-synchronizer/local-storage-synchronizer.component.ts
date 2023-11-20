6f49a11258cc8dbafbedcb8c9cde81d392bba035
Vlad Berezan <vlad.berezan@externals.adidas.com>
2023-11-20 21:34:04 +0200

    add local synchronizer

--- angular/toolbox-app/src/app/modules/local-storage/local-storage-synchronizer/component/local-storage-synchronizer/local-storage-synchronizer.component.ts
+++ angular/toolbox-app/src/app/modules/local-storage/local-storage-synchronizer/component/local-storage-synchronizer/local-storage-synchronizer.component.ts
@@ -20,6 +20,7 @@ export class LocalStorageSynchronizerComponent implements AfterViewInit {
   ngAfterViewInit(): void {
     this.delay(this.localStorageService.get(LocalStorageKeys.POST_INSTALL_FETCH_METADATA) ? 0 : 1000).then((): void => {
       let updates: number = 0;
+      console.log(this.localStorageService.get(LocalStorageKeys.POST_INSTALL_FETCH_METADATA));
       this.synchronizerService.updatesEmitter.subscribe((updateNumber: number): void => {
         updates += updateNumber;
         if (updates == 0) {
