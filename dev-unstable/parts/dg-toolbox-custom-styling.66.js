5060f0707dbec324e4b840f5db2e3bd52ae1ae95
Vlad Berezan <vlad.berezan@externals.adidas.com>
2023-11-22 10:49:30 +0200

    add local synchronizer

--- dev-unstable/parts/dg-toolbox-custom-styling.66.js
+++ dev-unstable/parts/dg-toolbox-custom-styling.66.js
@@ -42,8 +42,11 @@ function applyCustomStyling(windowURL) {
 
             let linkToNavigation = document.createElement('div');
             linkToNavigation.classList.add('dgt-link-to-navigation', 'right', 'resource');
-            linkToNavigation.innerHTML = '<a title="Navigation" href="/navigation/' + coords[0] + '/' + coords[1] + '/' + coords[2] + '/' + '"><img src="https://i.imgur.com/HcC4l22.png" width="16" height="16"></a>';
-            news.parentElement.append(linkToNavigation);
+
+            if (coords[0] !== '0') {
+                linkToNavigation.innerHTML = '<a target="_blank" title="Navigation" href="/navigation/' + coords[0] + '/' + coords[1] + '/' + coords[2] + '/' + '"><img src="https://i.imgur.com/HcC4l22.png" width="16" height="16"></a>';
+                news.parentElement.append(linkToNavigation);
+            }
         });
     }
 
