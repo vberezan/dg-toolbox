d39070e1a03a18d61073be4b52b39980adae71b5
Vlad Berezan <vlad.berezan@externals.adidas.com>
2023-11-22 12:10:34 +0200

    add local synchronizer

--- dev-unstable/dg-toolbox-development.user.js
+++ dev-unstable/dg-toolbox-development.user.js
@@ -27,7 +27,7 @@ function loadResource(element) {
 
 function getVersion() {
   if (localStorage.getItem('js-version')) return JSON.parse(localStorage.getItem('js-version')).value;
-  else return 'v2.0.2';
+  else return 'v2.0.4';
 }
 
 function loadSetups(windowURL) {
