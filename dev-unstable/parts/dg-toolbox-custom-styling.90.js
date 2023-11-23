9859b5312c15cc16613e70d84f0453381a361e23
Vlad Berezan <vlad.berezan@externals.adidas.com>
2023-11-23 15:59:46 +0200

    add local synchronizer

--- dev-unstable/parts/dg-toolbox-custom-styling.90.js
+++ dev-unstable/parts/dg-toolbox-custom-styling.90.js
@@ -728,7 +728,7 @@ function applyCustomStyling(windowURL) {
                 splitButton.classList.add('dgt-max-button');
                 splitButton.innerHTML = '<span><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M246.6 150.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l96-96c12.5-12.5 32.8-12.5 45.3 0l96 96c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L352 109.3V384c0 35.3 28.7 64 64 64h64c17.7 0 32 14.3 32 32s-14.3 32-32 32H416c-70.7 0-128-57.3-128-128c0-35.3-28.7-64-64-64H109.3l41.4 41.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0l-96-96c-12.5-12.5-12.5-32.8 0-45.3l96-96c12.5-12.5 32.8-12.5 45.3 0s12.5 32.8 0 45.3L109.3 256H224c23.3 0 45.2 6.2 64 17.1V109.3l-41.4 41.4z"/></svg></span>';
 
-                input.prepend(splitButton, input.querySelector('.text'));
+                input.insertBefore(splitButton, input.querySelector('.text'));
                 input.insertBefore(maxButton, splitButton);
 
                 splitButton.onmousedown = () => {
@@ -739,7 +739,7 @@ function applyCustomStyling(windowURL) {
                   }
                 }
               } else {
-                input.prepend(maxButton, input.querySelector('.text'));
+                input.insertBefore(maxButton, input.querySelector('.text'));
               }
 
 
@@ -773,7 +773,7 @@ function applyCustomStyling(windowURL) {
                 splitButton.classList.add('dgt-max-button');
                 splitButton.innerHTML = '<span><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M246.6 150.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l96-96c12.5-12.5 32.8-12.5 45.3 0l96 96c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L352 109.3V384c0 35.3 28.7 64 64 64h64c17.7 0 32 14.3 32 32s-14.3 32-32 32H416c-70.7 0-128-57.3-128-128c0-35.3-28.7-64-64-64H109.3l41.4 41.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0l-96-96c-12.5-12.5-12.5-32.8 0-45.3l96-96c12.5-12.5 32.8-12.5 45.3 0s12.5 32.8 0 45.3L109.3 256H224c23.3 0 45.2 6.2 64 17.1V109.3l-41.4 41.4z"/></svg></span>';
 
-                input.insertBefore(splitButton, input.querySelector('.text'));
+                input.prepend(splitButton, input.querySelector('.text'));
                 input.insertBefore(maxButton, splitButton);
 
                 splitButton.onmousedown = () => {
@@ -784,7 +784,7 @@ function applyCustomStyling(windowURL) {
                   }
                 }
               } else {
-                input.insertBefore(maxButton, input.querySelector('.text'));
+                input.prepend(maxButton, input.querySelector('.text'));
               }
 
 
