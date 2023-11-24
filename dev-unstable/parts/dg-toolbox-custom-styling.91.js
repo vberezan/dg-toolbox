4c6a65d32c3e659e77c2c11a65358799e689598d
Vlad Berezan <vlad.berezan@externals.adidas.com>
2023-11-24 10:40:59 +0200

    add local synchronizer

--- dev-unstable/parts/dg-toolbox-custom-styling.91.js
+++ dev-unstable/parts/dg-toolbox-custom-styling.91.js
@@ -204,19 +204,20 @@ function applyCustomStyling(windowURL) {
       document.querySelector('#planetImage').prepend(dgtNewCoords);
 
       let planetName = document.querySelector('.planetName');
-      let prev = detach(planetName.querySelector('a'));
-      prev.classList.add('dgt-planet-prev');
-      let next = detach(planetName.querySelector('a'));
-      next.classList.add('dgt-planet-next');
       let name = document.createElement('span');
       name.classList.add('dgt-planet-name');
       name.innerHTML = planetName.textContent.trim();
 
-      if (next) {
+      let prev = detach(planetName.querySelector('a'));
+      let next = detach(planetName.querySelector('a'));
+
+      if (next != null) {
+        next.classList.add('dgt-planet-next');
         next.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><style>svg{fill:#ffffff}</style><path d="M464 256A208 208 0 1 1 48 256a208 208 0 1 1 416 0zM0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM294.6 135.1c-4.2-4.5-10.1-7.1-16.3-7.1C266 128 256 138 256 150.3V208H160c-17.7 0-32 14.3-32 32v32c0 17.7 14.3 32 32 32h96v57.7c0 12.3 10 22.3 22.3 22.3c6.2 0 12.1-2.6 16.3-7.1l99.9-107.1c3.5-3.8 5.5-8.7 5.5-13.8s-2-10.1-5.5-13.8L294.6 135.1z"></path></svg>';
       }
 
-      if (prev) {
+      if (prev != null) {
+        prev.classList.add('dgt-planet-prev');
         prev.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><style>svg{fill:#ffffff}</style><path d="M48 256a208 208 0 1 1 416 0A208 208 0 1 1 48 256zm464 0A256 256 0 1 0 0 256a256 256 0 1 0 512 0zM217.4 376.9c4.2 4.5 10.1 7.1 16.3 7.1c12.3 0 22.3-10 22.3-22.3V304h96c17.7 0 32-14.3 32-32V240c0-17.7-14.3-32-32-32H256V150.3c0-12.3-10-22.3-22.3-22.3c-6.2 0-12.1 2.6-16.3 7.1L117.5 242.2c-3.5 3.8-5.5 8.7-5.5 13.8s2 10.1 5.5 13.8l99.9 107.1z"></path></svg>';
       }
 
