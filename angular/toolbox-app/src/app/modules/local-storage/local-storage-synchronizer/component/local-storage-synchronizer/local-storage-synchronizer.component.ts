d2f805112dffca6f567feffc1364eb116afc1e33
Vlad Berezan <vlad.berezan@externals.adidas.com>
2023-11-25 11:38:31 +0200

    add local synchronizer

--- angular/toolbox-app/src/app/modules/local-storage/local-storage-synchronizer/component/local-storage-synchronizer/local-storage-synchronizer.component.ts
+++ angular/toolbox-app/src/app/modules/local-storage/local-storage-synchronizer/component/local-storage-synchronizer/local-storage-synchronizer.component.ts
@@ -39,11 +39,6 @@ export class LocalStorageSynchronizerComponent implements AfterViewInit {
   }
 
   ngAfterViewInit(): void {
-    let audio = new Audio();
-    audio.src = "https://github.com/vberezan/dg-toolbox/blob/development/angular/toolbox-app/src/assets/sounds/bts-operational.mp3";
-    audio.load();
-    audio.play();
-
     this.metadataService.resetSortingMaps();
     this.prepareRankingsLoader();
 
