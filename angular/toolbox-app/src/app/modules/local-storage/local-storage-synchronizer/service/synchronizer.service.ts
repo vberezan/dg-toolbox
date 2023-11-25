eae350e0e65d339c56638e82164a219e15b505e7
Vlad Berezan <vlad.berezan@externals.adidas.com>
2023-11-25 10:01:53 +0200

    add local synchronizer

--- angular/toolbox-app/src/app/modules/local-storage/local-storage-synchronizer/service/synchronizer.service.ts
+++ angular/toolbox-app/src/app/modules/local-storage/local-storage-synchronizer/service/synchronizer.service.ts
@@ -179,6 +179,7 @@ export class SynchronizerService {
                 }
 
                 cache.push(allianceMember);
+                console.log(allianceMember);
               }
             });
           }
