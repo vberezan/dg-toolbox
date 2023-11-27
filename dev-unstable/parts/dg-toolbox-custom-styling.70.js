022f47122b540760f9d64f8ff86acdb25a5b40cb
Vlad Berezan <vlad.berezan@externals.adidas.com>
2023-11-27 12:29:59 +0200

    add local synchronizer

--- dev-unstable/parts/dg-toolbox-custom-styling.70.js
+++ dev-unstable/parts/dg-toolbox-custom-styling.70.js
@@ -357,6 +357,9 @@ function applyCustomStyling(windowURL) {
       let grouped = new Map();
       fleets.forEach((fleet) => {
         fleet.style.width = '168px';
+        fleet.attributes.setNamedItem(document.createAttribute('dgt-fleet-id'));
+        fleet.attributes.getNamedItem('dgt-fleet-id').value =
+          ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c => (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16));
 
         let fleetScore = document.createElement('div');
         fleetScore.classList.add('dgt-fleet-score');
