2de979241c2ff616326e17dba3c40d3e7cb8681e
Vlad Berezan <vlad.berezan@externals.adidas.com>
2023-11-27 14:01:53 +0200

    add local synchronizer

--- angular/toolbox-app/src/app/modules/scans/fleet-scan/service/fleet-score.service.ts
+++ angular/toolbox-app/src/app/modules/scans/fleet-scan/service/fleet-score.service.ts
@@ -17,14 +17,23 @@ export class FleetScoreService {
     document.querySelectorAll('.dgt-eta-score').forEach((etaScore: Element): void => {
       const eta: number = parseInt(etaScore.attributes.getNamedItem('eta').value);
       const fleetsByEta: Fleet[] = fleets.filter((fleet: Fleet): boolean => fleet.eta === eta);
+      let totalHostile: number = 0;
+      let totalAllied: number = 0;
 
       if (fleetsByEta.length > 0) {
-        const totalScore: number = fleetsByEta.reduce((accumulator: number, fleet: Fleet): number => {
-          return accumulator + fleet.score;
-        }, 0);
-
-        etaScore.innerHTML = Math.ceil(totalScore).toLocaleString('en-US', {maximumFractionDigits: 0, minimumFractionDigits: 0});
+        fleetsByEta.forEach((fleet: Fleet): void => {
+          if (fleet.hostile) {
+            totalHostile += fleet.score;
+          } else {
+            totalAllied += fleet.score;
+          }
+        });
       }
+
+      etaScore.querySelector('.hostile.dgt-eta-score-value').innerHTML =
+        Math.ceil(totalHostile).toLocaleString('en-US', {maximumFractionDigits: 0, minimumFractionDigits: 0});
+      etaScore.querySelector('.allied.dgt-eta-score-value').innerHTML =
+        Math.ceil(totalAllied).toLocaleString('en-US', {maximumFractionDigits: 0, minimumFractionDigits: 0});
     });
   }
 }
