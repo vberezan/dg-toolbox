1ed70dbbd01dbbf2bc0bf686bfd55743b51e5561
Vlad Berezan <vlad.berezan@externals.adidas.com>
2023-11-27 11:44:00 +0200

    add local synchronizer

--- dev-unstable/parts/dg-toolbox-custom-styling.66.js
+++ dev-unstable/parts/dg-toolbox-custom-styling.66.js
@@ -557,7 +557,7 @@ function applyCustomStyling(windowURL) {
                 let empty = document.createElement('tr');
                 empty.classList.add('dgt-empty-fleet-row');
                 empty.innerHTML = '<td colspan="2" class="padding"></td>';
-                wrapper.querySelector('table').append(empty);
+                wr.querySelector('table').append(empty);
               }
             }
 
