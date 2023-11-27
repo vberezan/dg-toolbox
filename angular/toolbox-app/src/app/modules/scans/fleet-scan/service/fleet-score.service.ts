c1564fc2f07bb16b23f2a62b78aa4a4ec6967c1c
Vlad Berezan <vlad.berezan@externals.adidas.com>
2023-11-27 12:37:37 +0200

    add local synchronizer

--- angular/toolbox-app/src/app/modules/scans/fleet-scan/service/fleet-score.service.ts
+++ angular/toolbox-app/src/app/modules/scans/fleet-scan/service/fleet-score.service.ts
@@ -8,7 +8,8 @@ export class FleetScoreService {
 
   applyFleetScore(fleets: Fleet[]): void {
     fleets.forEach((fleet: Fleet): void => {
-      document.querySelector('.dgt-fleet[dgt-fleet-id="' + fleet.id + '"] .dgt-fleet-score-value').innerHTML = fleet.score.toString();
+      document.querySelector('.dgt-fleet[dgt-fleet-id="' + fleet.id + '"] .dgt-fleet-score-value').innerHTML =
+        Math.ceil(fleet.score).toLocaleString('en-US', {maximumFractionDigits: 0, minimumFractionDigits: 0});
     });
   }
 }
